63feea1e893abbb38e6dcdaf249d2d39ddf442e8
Radi3nt <pro.radi3nt@gmail.com>
2022-12-06 22:20:40 +0100

    New math library (some matrix methods untested, WIP)

---
+++ MathsHelper/src/main/java/fr/radi3nt/maths/components/advanced/matrix/Matrix4x4.java
@@ -0,0 +1,37 @@
+package fr.radi3nt.maths.components.advanced.matrix;
+
+import fr.radi3nt.maths.components.advanced.matrix.angle.Angle;
+import fr.radi3nt.maths.components.advanced.quaternions.Quaternion;
+import fr.radi3nt.maths.components.vectors.Vector3f;
+import fr.radi3nt.maths.components.vectors.Vector4f;
+
+public interface Matrix4x4 extends Matrix {
+
+    Quaternion getRotation();
+
+    Vector3f getTranslation();
+
+    Vector3f getScale();
+
+    void translation(Vector3f translation);
+
+    void scale(Vector3f scale);
+
+    void rotationX(Angle angle);
+
+    void rotationY(Angle angle);
+
+    void rotationZ(Angle angle);
+
+    void eulerRotation(Angle x, Angle y, Angle z);
+
+    void quaternionRotation(Quaternion quaternion);
+
+    void directionRotation(Vector3f direction, Vector3f up);
+
+    void transform(Vector4f toTransform);
+
+    void multiply(Matrix matrix);
+
+    void copy(Matrix result);
+}
---
+++ MathsHelper/src/main/java/fr/radi3nt/maths/components/advanced/matrix/MatrixTesting.java
@@ -0,0 +1,10 @@
+package fr.radi3nt.maths.components.advanced.matrix;
+
+public class MatrixTesting {
+
+    public static void main(String[] args) {
+        ArrayMatrix4x4 arrayMatrix4x4 = new ArrayMatrix4x4();
+        arrayMatrix4x4.zero();
+    }
+
+}
