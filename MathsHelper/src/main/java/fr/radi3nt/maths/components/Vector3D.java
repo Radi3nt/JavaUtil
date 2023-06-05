3b4fd1c86b8c9eea1c5c94f07f4d76a26452ad06
Radi3nt <pro.radi3nt@gmail.com>
2023-06-05 14:50:45 +0200

    Fixed issue in Vector3D

--- MathsHelper/src/main/java/fr/radi3nt/maths/components/Vector3D.java
+++ MathsHelper/src/main/java/fr/radi3nt/maths/components/Vector3D.java
@@ -36,8 +36,8 @@ public class Vector3D implements Cloneable {
 
     public Vector3D(Vector3f vector) {
         this.x = vector.getX();
-        this.y = vector.getX();
-        this.z = vector.getX();
+        this.y = vector.getY();
+        this.z = vector.getZ();
     }
 
     public Vector3D add(Vector3D vec) {
