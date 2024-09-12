43456da5059f2b5b92c9c3127203038a6fa1da59
Radi3nt <pro.radi3nt@gmail.com>
2024-09-12 22:51:33 +0200

    Made variable in WriterConfig final

--- JsonHelper/src/main/java/fr/radi3nt/json/WriterConfig.java
+++ JsonHelper/src/main/java/fr/radi3nt/json/WriterConfig.java
@@ -3,18 +3,14 @@ package fr.radi3nt.json;
 import java.io.Writer;
 
 public abstract class WriterConfig {
-    public static WriterConfig MINIMAL;
-    public static WriterConfig PRETTY_PRINT;
 
-    static {
-        WriterConfig.MINIMAL = new WriterConfig() {
-            @Override
-            JsonWriter createWriter(final Writer writer) {
-                return new JsonWriter(writer);
-            }
-        };
-        WriterConfig.PRETTY_PRINT = PrettyPrint.indentWithSpaces(2);
-    }
+    public static final WriterConfig MINIMAL = new WriterConfig() {
+        @Override
+        JsonWriter createWriter(final Writer writer) {
+            return new JsonWriter(writer);
+        }
+    };
+    public static final WriterConfig PRETTY_PRINT = PrettyPrint.indentWithSpaces(2);
 
     abstract JsonWriter createWriter(final Writer p0);
 }
